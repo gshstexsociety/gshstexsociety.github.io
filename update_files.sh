eeae62d5382f0d2cc36c9e1f8ff5f81cf8e806cd
Seungwon Park <psw14041@gmail.com>
2016-08-29 17:44:43 +0900

    Files -> files

--- update_files.sh
+++ update_files.sh
@@ -6,25 +6,24 @@ cd /var/www/html
 git pull
 
 echo Repo 1...
-cd /var/www/LaTeX_Files/An-Introduction-to-LaTeX
+cd /var/www/LaTeX_files/An-Introduction-to-LaTeX
 git pull
 zip -FSr ../An-Introduction-to-LaTeX.zip *
 
 echo Repo 2...
-cd /var/www/LaTeX_Files/examples
+cd /var/www/LaTeX_files/examples
 git pull
 zip -FSr ../examples.zip *
 
 echo Repo 3...
-cd /var/www/LaTeX_Files/gshslatexintro
+cd /var/www/LaTeX_files/gshslatexintro
 git pull
 zip -FSr ../gshslatexintro.zip *
 
 echo Repo 4...
-cd /var/www/LaTeX_Files/pdf-cloud
+cd /var/www/LaTeX_files/pdf-cloud
 git pull
 zip -FSr ../pdf-cloud.zip *
 
 export TZ=Asia/Seoul
-date > /home/pi/web/gshs/LaTeX/updated.txt
 
