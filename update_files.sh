79baa6db2aa988465b0ba8361f72bca273f7456e
Seungwon Park <psw14041@gmail.com>
2016-08-29 17:40:27 +0900

    Changed directory

--- update_files.sh
+++ update_files.sh
@@ -2,26 +2,26 @@ echo Starting...
 date
 
 echo PHP Site...
-cd /home/pi/web/gshs/LaTeX
+cd /var/www/html
 git pull
 
 echo Repo 1...
-cd /home/pi/web/gshs/LaTeX_Files/An-Introduction-to-LaTeX
+cd /var/www/LaTeX_Files/An-Introduction-to-LaTeX
 git pull
 zip -FSr ../An-Introduction-to-LaTeX.zip *
 
 echo Repo 2...
-cd /home/pi/web/gshs/LaTeX_Files/examples
+cd /var/www/LaTeX_Files/examples
 git pull
 zip -FSr ../examples.zip *
 
 echo Repo 3...
-cd /home/pi/web/gshs/LaTeX_Files/gshslatexintro
+cd /var/www/LaTeX_Files/gshslatexintro
 git pull
 zip -FSr ../gshslatexintro.zip *
 
 echo Repo 4...
-cd /home/pi/web/gshs/LaTeX_Files/pdf-cloud
+cd /var/www/LaTeX_Files/pdf-cloud
 git pull
 zip -FSr ../pdf-cloud.zip *
 
