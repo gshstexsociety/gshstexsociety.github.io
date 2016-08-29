413b929661eaf6815f9ae51c34441237bc96ce59
Seungwon Park <psw14041@gmail.com>
2016-08-29 18:06:40 +0900

    asdf

--- update_files.sh
+++ update_files.sh
@@ -13,17 +13,17 @@ zip -FSr /var/www/html/files/An-Introduction-to-LaTeX.zip *
 echo Repo 2...
 cd /var/www/html/files/examples
 git pull
-zip -FSr /var/www/html/examples.zip *
+zip -FSr /var/www/html/files/examples.zip *
 
 echo Repo 3...
 cd /var/www/html/files/gshslatexintro
 git pull
-zip -FSr /var/www/html/gshslatexintro.zip *
+zip -FSr /var/www/html/files/gshslatexintro.zip *
 
 echo Repo 4...
 cd /var/www/html/files/pdf-cloud
 git pull
-zip -FSr /var/www/html/pdf-cloud.zip *
+zip -FSr /var/www/html/files/pdf-cloud.zip *
 
 export TZ=Asia/Seoul
 date > /var/www/html/updated.txt
