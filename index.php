78f1809d25be686f75d2d4b6c24e685b8b0d197a
Seungwon Park <psw14041@gmail.com>
2016-09-05 13:28:59 +0900

    Update index.php

--- index.php
+++ index.php
@@ -59,17 +59,15 @@
 <h4> 각종 양식들 - tex 또는 cls 파일로 제공됩니다. </h4>
 <h4> 모든 양식은 TeXstudio 사용자를 기준으로 제작됩니다. </h4>
 
-<li> <b>GSHS RnE report class</b> : 기초/심화 R&E tex 양식 및 샘플 (<b>정민석</b>, 박승원, 이상헌) </li>
-<li> <b>gshs_thesis_certified</b> : (공식) 졸업논문 R&E tex 샘플(겸 논문작성 가이드) 및 cls 파일 (<b>목진욱</b>, 박승원, 이주찬) </li>
-<li> gshs_thesis_certified-<b>adv</b> : BibTeX, subfiles, xelatex 을 이용한 졸업논문 양식 (<b>이주찬</b>, 박승원) </li>
-<li> sample_document : 단순한 샘플 문서. User template 으로 등록해두면 편합니다. (박승원) </li>
+<li> <b>gshs_report</b> : 기초/심화 R&E tex 양식(<b>정민석</b>, 박승원, 이상헌) </li>
+<li> <b>gshs_thesis</b> : 졸업논문 R&E tex 양식(<b>목진욱</b>, 박승원, 이주찬) </li>
+<li> gshs_thesis-<b>adv</b> : BibTeX, subfiles, xelatex 을 이용한 간편한 졸업논문 양식 (<b>이주찬</b>, 박승원) </li>
 <li> <b>gshs_beamer</b> : 다용도 Beamer Template. (<b>목진욱</b>, 박승원) </li>
-<li> <b>gshs_chem_experiment</b> : 일반화학실험 실험보고서 도움말 및 cls 파일(beta, 2016.01.13) (<b>이주찬</b>) </li>
+<li> <b>gshs_chem_experiment</b> : 일반화학실험 실험보고서 도움말 및 양식(beta, 2016.01.13) (<b>이주찬</b>) </li>
 <li> <b>Humantech_Paper_Award</b> : 휴먼테크논문대회 초록/논문 양식(2015년도-22회 기준) (<b>목진욱</b>, 박승원) </li>
-<li> Jointed_RnE_presentation : 영재학교 우수 R&E 공동발표회 보고서 양식(제 5회(대전과학고) 기준) (<b>박승원</b>) </li>
+<li> 5th_jointed_rne_presentation : 영재학교 우수 R&E 공동발표회 보고서 양식(제 5회(대전과학고) 기준) (<b>박승원</b>) </li>
 <li> gshs_reading : 2016학년도 1학기 독서 독후감 양식 (<b>이주찬</b>)</li>
 <li> gshs_math_seminar_report : 2016학년도 수학세미나I 보고서 양식 (<b>박승원</b>) </li>
-<li> gshs_poster : (Beta) 경기과학고 전용 포스터 양식 (<b>박승원</b>) </li>
 <li> gshs_imaginarylab : 무한상상캠프 작품제작설명서 양식 (<b>박승원</b>, 서울) </li>
 
 
@@ -78,17 +76,17 @@
 <li> Discrete_Data_Prob_14080 : 이산구조 레포트 (이상헌) </li>
 <li> Korean_and_Life_14041 : 국어와생활 시험범위 정리 (박승원) </li>
 <li> 2015_Science_Research_Writing : 영어논문작성법 시험범위 정리 (박승원, 이상헌, 이주찬) </li>
-<li> Advanced_Physics_14041 : 고급물리학I 레포트 </li>
-<li> 2016_GSHS_Linear_Algebra_midterm : Linear Algebra Thm Archive - for Mid-Term Exam(황동욱, 박승원) </li>
+<li> Advanced_Physics_14041 : 고급물리학I 레포트 (박승원) </li>
+<li> 2016_GSHS_Linear_Algebra_midterm : Linear Algebra Thm Archive - for Mid-Term Exam(<b>황동욱</b>, 박승원) </li>
 <li> Manany_Harmony_Project : 경기과학고 작곡작사동아리 매나니 화성학 강의(이상헌) </li>
 <li> Science_In_Narratives_14080 : 경기과학고 'Science in Narratives' 과목 발표자료(이상헌) </li>
+<li> sample_document : 단순한 샘플 문서. User template 으로 등록해두면 편합니다. (박승원) </li>
 
 <h3> LaTeX 시작하기 </h3>
 <h4> TeX 엔진 설치 </h4>
 <p>TeX 을 사용하기 위해서는 TeXlive 를 설치해야 합니다.
 <a href="https://github.com/gshslatexintro/An-Introduction-to-LaTeX/blob/master/Installation.md" target="_blank">TeXlive 설치 가이드</a>
-
-용량이 2GB정도 되니 주의 바랍니다. 설치 후 차지하는 용량은 약 3GB.</p>
+</p>
 
 <h4> TeX 에디터 설치 </h4>
 <p>TeXstudio를 강력 추천합니다. <a href="http://www.texstudio.org/" target="_blank">http://www.texstudio.org/</a></p>
