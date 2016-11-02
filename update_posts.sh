40c9f8be1cc952f5daefe4765a4685cb0666309e
root <root@localhost.localdomain>
2016-11-02 16:28:45 +0900

    Update uncommited change - update_posts.sh

--- update_posts.sh
+++ update_posts.sh
@@ -1,9 +1,7 @@
-#PATH=/bin/date:/usr/local/bin:/usr/local/rvm/gems/ruby-2.3.0/bin:/usr/bin
-
 echo Started updating posts...
 date
 
 echo Updating posts and website...
 cd /var/www/source
 git pull
-sudo -E jekyll build
+jekyll build
