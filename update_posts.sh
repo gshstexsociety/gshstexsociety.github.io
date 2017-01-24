0f121da218c205a69ce21fb0a804cbbfeb4f40b8
Seungwon Park <psw14041@gmail.com>
2017-01-25 00:25:42 +0900

    examples-mirror git clone

--- update_posts.sh
+++ update_posts.sh
@@ -5,3 +5,6 @@ echo Updating posts and website...
 cd /var/www/source
 git pull
 jekyll build
+
+cd /var/www/source/files
+git clone https://github.com/gshslatexintro/examples-mirror examples-mirror
