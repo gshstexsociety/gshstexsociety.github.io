e2720eb9d2358b9b7e984c19eaf9d259ad51993e
Hyunin Song <tteu.ingog@gmail.com>
2017-10-13 14:40:13 +0900

    Do the same thing for update_posts.sh

--- update_posts.sh
+++ update_posts.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/env bash
+
+find_branch="git symbolic-ref -q HEAD"
+find_upstream="git for-each-ref --format=%(upstream:short)"
+
 echo Started updating posts...
 date
 
 echo Updating posts and website...
 cd /var/www/source
-git pull
+git fetch --all
+git reset --hard $($find_upstream $($find_branch))
 jekyll build
