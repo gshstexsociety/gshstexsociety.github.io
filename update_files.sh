d2ceaaeb052d000da59f496d572a8834b0d15f4d
Hyunin Song <tteu.ingog@gmail.com>
2017-10-13 14:13:56 +0900

    Use hard reset instead of pull

--- update_files.sh
+++ update_files.sh
@@ -1,5 +1,10 @@
-# Repo information
+#!/usr/bin/env bash
+# Git repo information
+org_url=https://github.com/gshslatexintro
 repo_list="An-Introduction-to-LaTeX examples examples-mirror gshs-format gshs-format-mirror"
+
+find_branch="git symbolic-ref -q HEAD"
+find_upstream="git for-each-ref --format=%(upstream:short)"
 
 echo Started updating files...
 date
@@ -9,10 +14,19 @@ cd /var/www/source/files/
 for repo in $repo_list; do
 echo Repo $repo...
 if [ ! -d $repo ]; then
-git clone https://github.com/gshslatexintro/$repo
+git clone $org_url/$repo
 fi
 pushd $repo
-git pull
-zip -FSr ../$repo.zip /*
+git fetch --all
+echo Checkout $($find_upstream $($find_branch))...
+git reset --hard $($find_upstream $($find_branch))
+zip -FSr ../$repo.zip *
 popd
 done
+
+echo Time...
+date > /var/www/source/updated.txt
+
+echo Building website...
+cd /var/www/source
+jekyll build
